fe4502ae4c28a5972f64ff9bf6c0559f209b13f5
ankitachaubey12 <anikitachaubey@logiceasetecno.com>
2023-09-22 14:48:57 +0530

    add dimens

---
+++ designToken/dimens.tsx
@@ -0,0 +1,14 @@
+export const dimensions = {
+    // Margins and paddings
+    marginSm: 10,
+    marginMd: 20,
+    marginLg: 30,
+    paddingSm: 8,
+    paddingMd: 16,
+    paddingLg: 24,
+
+    // Border widths
+    borderWidthThin: 1,
+    borderWidthMedium: 2,
+    borderWidthThick: 3,
+}
