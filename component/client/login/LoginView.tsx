4064b0868cf7640619f5c38998f12fb01f935ef8
Gurpreet Saini <gurpreetsaini@logiceasetecno.com>
2023-09-26 11:41:31 +0530

    base code setup

---
+++ component/client/login/LoginView.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginView = () => {
+  return (
+    <View>
+      <Text>LoginView</Text>
+    </View>
+  )
+}
+
+export default LoginView
+
+const styles = StyleSheet.create({})
---
+++ component/client/registration/RegistrationView.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegistrationView = () => {
+  return (
+    <View>
+      <Text>RegistrationView</Text>
+    </View>
+  )
+}
+
+export default RegistrationView
+
+const styles = StyleSheet.create({})
