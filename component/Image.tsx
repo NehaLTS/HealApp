b3afac9c10548a27639e64a2317e36811c95fc96
ankitachaubey12 <anikitachaubey@logiceasetecno.com>
2023-09-27 11:23:35 +0530

    components changes

--- component/Image.tsx
+++ component/Image.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Image as RNImage, ImageSourcePropType, ImageProps } from 'react-native'
+import { StyleSheet, Image as RNImage, ImageSourcePropType, ImageProps, ViewStyle } from 'react-native'
 import React from 'react'
 
-const Image = ({ source, ...props }: { source: ImageSourcePropType } & ImageProps) => {
-    return <RNImage source={source} {...props} />
+const Image = ({ source, width, height, ...props }: { source: ImageSourcePropType; width?: number; height?: number } & ImageProps) => {
+    const imageStyle: ViewStyle = {
+        width: width || 247, 
+        height: height || 252, 
+    };
+
+    return <RNImage source={source}  style={[imageStyle, styles.image]}  {...props} />
 }
 
 export default Image
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{
+
+    }
+})
--- component/Input.tsx
+++ component/Input.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Image, StyleSheet, TextInput, TextInputProps, TouchableOpacity, View } from 'react-native';
-import { lightColors } from '../designToken/colors';
+import { colors } from '../designToken/colors';
 import { fontSize } from '../designToken/fontSizes';
+import { getHeight, getWidth } from '../libs/StyleHelper';
+import showSvg from '../designToken/svg/show.svg'
+import { SvgUri } from 'react-native-svg';
 
-const Input = ({ placeholder, type, onShowPasward, ...props }: { placeholder: string, type?: any, onShowPasward?: () => void } & TextInputProps) => {
+const Input = ({ placeholder, type, onShowPassword, ...props }: { placeholder: string, type?: any, onShowPassword?: () => void } & TextInputProps) => {
 
   return (
     <View style={styles.inputContainer}>
@@ -12,13 +15,16 @@ const Input = ({ placeholder, type, onShowPasward, ...props }: { placeholder: st
         placeholder={placeholder}
         placeholderTextColor="rgba(0, 0, 0, 1)"
         textContentType={type ?? 'name'}
+        secureTextEntry={type === 'password'}
         {...props}
       />
-      {type !== 'password' &&
-        <TouchableOpacity onPress={onShowPasward}>
-          <Image
-            source={require('../designToken/svg/show.png')}
-            style={styles.showImage}
+      {type === 'password' &&
+        <TouchableOpacity onPress={onShowPassword}>
+          <SvgUri
+            width={40}
+            height={40}
+            uri={showSvg}
+         
           />
         </TouchableOpacity>}
     </View>
@@ -28,21 +34,19 @@ const Input = ({ placeholder, type, onShowPasward, ...props }: { placeholder: st
 const styles = StyleSheet.create({
   inputContainer: {
     alignItems: 'center',
-    borderWidth: 2,
-    borderColor: lightColors.primary,
-    borderRadius: 10,
+    borderWidth: getWidth(2),
+    borderColor: colors.primary,
+    borderRadius: getWidth(10),
     flexDirection:'row',
-    height: 48,
+    height: getHeight(48),
   },
   input: {
     fontSize: fontSize.textLg,
     padding: 10,
-    flex:1
+    flex:1,
+    color:"black"
   },
-  showImage: {
-    width: 30,
-    height: 30,
-  },
+
 
 });
 
