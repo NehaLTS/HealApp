36e43e4109625f255b675b017b9ca6bbf68d7892
NehaLTS <104347575+NehaLTS@users.noreply.github.com>
2023-09-28 14:42:20 +0530

    Removed demo Google login button

--- screens/IntroScreen.tsx
+++ screens/IntroScreen.tsx
@@ -47,8 +47,6 @@ const IntroScreen = () => {
             zIndex: 1
           }}
         >
-         <GoogleButtonView />
-
           <Text
             style={{
               fontSize: getHeight(24),
