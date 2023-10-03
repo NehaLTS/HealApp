2dcbfad1e16a6a8eac67afd5fe62d47136fbe275
ankitachaubey12 <anikitachaubey@logiceasetecno.com>
2023-10-03 13:48:04 +0530

    swiper

--- screens/onboard/OnboardControl.tsx
+++ screens/onboard/OnboardControl.tsx
@@ -1,13 +1,28 @@
-import { SetStateAction } from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import OnboardView from './OnboadrView';
+import Swiper from 'react-native-swiper';
 
-// OnboardControl.js
-export const startChangingText = (setCircleText: { (value: SetStateAction<number>): void; (arg0: number): void; }) => {
-  let counter = 1;
-  const interval = setInterval(() => {
-    if (counter === 3) {
-      clearInterval(interval); // Stop changing text after reaching the desired count.
-    }
-    setCircleText(counter);
-    counter++;
-  }, 5000); // Change every 5 seconds.
+const OnboardControl = () => {
+  const swipeRef = useRef<Swiper | null>(null); // Declare the type for swiperRef
+  const [currentIndex, setCurrentIndex] = useState<number>(0); // Declare the type for currentIndex as number
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (swipeRef.current) {
+        const nextIndex = (currentIndex + 1) % 3;
+        swipeRef.current.scrollBy(nextIndex - currentIndex, true);
+        setCurrentIndex(nextIndex);
+      }
+    }, 3000); // 3 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentIndex]);
+
+  return <OnboardView currentIndex={currentIndex} />;
 };
+export default OnboardControl
+
+const styles = StyleSheet.create({})
