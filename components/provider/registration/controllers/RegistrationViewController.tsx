2305b17da2b78802b6229569d779573df881b1cd
“kamalpreetkaur001” <kamalpreet@logiceasetecno.com>
2023-10-09 18:10:23 +0530

    error fixes

--- components/provider/registration/controllers/RegistrationViewController.tsx
+++ components/provider/registration/controllers/RegistrationViewController.tsx
@@ -1,9 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import { useApiContext } from "contexts/useApiContext";
 import { Alert } from "react-native";
 
 const RegistrationViewController = () => {
-  const { onAuthSignUp } = useApiContext()
   const navigation = useNavigation()
   const onPressSignUp = async (email: string, password: string) => {
     const res = await onAuthSignUp?.(email, password);
