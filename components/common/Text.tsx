9ebc2e933c0a047a9dc7481afe97b81b5b7bbea8
Gurpreet Saini <gurpreetsaini@logiceasetecno.com>
2023-10-09 11:31:12 +0530

    intro screen ui fixes

---
+++ components/common/Text.tsx
@@ -0,0 +1,17 @@
+import { colors } from "designToken/colors";
+import { getWidth } from "libs/StyleHelper";
+import React from "react";
+import { Text as RNText, StyleProp, StyleSheet, TextStyle } from "react-native";
+
+const Text = ( {title, style}:{title: string, style?: StyleProp<TextStyle>}) => {
+  return <RNText style={[styles.text, style]}>{title}</RNText>;
+};
+
+export default Text;
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.black,
+    letterSpacing: getWidth(1.5),
+  },
+});
