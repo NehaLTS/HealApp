86926c781d1fdb9f7e0ae8140217745db8fb1440
Gurpreet Saini <gurpreetsaini@logiceasetecno.com>
2023-10-10 16:41:24 +0530

    added sizes

--- designToken/fontFamily.tsx
+++ designToken/fontFamily.tsx
@@ -1,8 +1,8 @@
 export const fontFamily = {
-    light: "Montserrat-Light",
-    regular: "Montserrat-Regular",
-    medium: "Montserrat-Medium",
-    semiBold:"Montserrat-SemiBold",
-    bold:"Montserrat-Bold",
-    black:"Montserrat-Black"
+    light: "Montserrat-Light", //300
+    regular: "Montserrat-Regular", //400
+    medium: "Montserrat-Medium", //500
+    semiBold:"Montserrat-SemiBold", //600
+    bold:"Montserrat-Bold", //700
+    black:"Montserrat-Black" //900
 } as const
