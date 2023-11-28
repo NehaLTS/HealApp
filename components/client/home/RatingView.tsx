c52c7edcccbc062ca668947cd8b09d50e45dbf96
ankitachaubey12 <anikitachaubey@logiceasetecno.com>
2023-11-28 16:03:36 +0530

    next 3 ui components

---
+++ components/client/home/RatingView.tsx
@@ -0,0 +1,118 @@
+import { StyleSheet, Image, View } from 'react-native';
+import React from 'react';
+import { getHeight, getWidth } from 'libs/StyleHelper';
+import Text from 'components/common/Text';
+import { dimens } from 'designToken/dimens';
+import { fontFamily } from 'designToken/fontFamily';
+import Input from 'components/common/Input';
+import { colors } from 'designToken/colors';
+import { fontSize } from 'designToken/fontSizes';
+
+const RatingView = () => {
+  const renderStars = (numStars: number) => {
+    const stars = Array.from({ length: numStars }).map((_, index) => (
+      <Image
+        key={index}
+        source={require('../../../assets/icon/ratingStar.png')}
+        style={styles.starIcon}
+      />
+    ));
+    return stars;
+  };
+
+  return (
+    <>
+      <View style={{ alignItems: 'center' }}>
+        <Image
+          source={require('../../../assets/icon/doctorIcon.png')}
+          style={styles.doctorIcon}
+        />
+        <Image
+          source={require('../../../assets/icon/star.png')}
+          style={styles.star}
+        />
+        <Text style={styles.rating} title={'4.8'} />
+      </View>
+      <View style={styles.container}>
+        <Text
+          title={'Elena Miron, family doctor'}
+          style={styles.familyDoctorText}
+        />
+        <Text
+          title={'How do you rate our doctor?'}
+          style={{ fontSize: getHeight(18) }}
+        />
+        <View style={styles.image}>{renderStars(5)}</View>
+      </View>
+
+      <Text
+        title={'Something else you want to tell us?'}
+        style={styles.tellText}
+      />
+      <Input
+        inputPlaceholder={'What did you like about our services?'}
+        inputStyle={styles.description}
+        placeholderTextColor={colors.grey}
+        style={styles.placeholder}
+      />
+    </>
+  );
+};
+
+export default RatingView;
+
+const styles = StyleSheet.create({
+  doctorIcon: {
+    width: getWidth(dimens.imageM + dimens.marginL),
+    height: getHeight(dimens.imageM + dimens.marginL),
+    resizeMode: 'contain',
+  },
+  starIcon: {
+    height: getHeight(dimens.marginL),
+    width: getWidth(dimens.marginL),
+  },
+  container: {
+    alignItems: 'center',
+    gap: getHeight(8),
+    marginTop: getHeight(dimens.marginL),
+  },
+  familyDoctorText: {
+    fontSize: getHeight(20),
+    fontFamily: fontFamily.medium,
+  },
+  image: {
+    flexDirection: 'row',
+    gap: getHeight(dimens.marginM),
+    marginTop: getHeight(dimens.marginM),
+    marginBottom: getHeight(dimens.imageS),
+  },
+  description: {
+    height: '40%',
+    backgroundColor: colors.white,
+  },
+
+  placeholder: {
+    marginTop: getHeight(-80),
+    paddingLeft: getWidth(dimens.marginS),
+    fontFamily: fontFamily.regular,
+    flex: 1,
+  },
+  rating: {
+    position: 'absolute',
+    bottom: getHeight(-11),
+    right: getWidth(dimens.imageS + dimens.imageS),
+    fontSize: getHeight(fontSize.textM),
+  },
+  star: {
+    position: 'absolute',
+    bottom: getHeight(dimens.marginS),
+    right: getWidth(dimens.imageS + dimens.imageS),
+    width: getWidth(dimens.marginM),
+    height: getHeight(dimens.marginM),
+  },
+  tellText: {
+    textAlign: 'center',
+    marginBottom: getHeight(dimens.marginS + 5),
+    fontSize: getHeight(fontSize.textL),
+  },
+});
--- components/client/home/TreatmentEnd.tsx
+++ components/client/home/TreatmentEnd.tsx
@@ -3,29 +3,68 @@ import React from 'react';
 import Text from 'components/common/Text';
 import { getHeight, getWidth } from 'libs/StyleHelper';
 import { dimens } from 'designToken/dimens';
+import { fontSize } from 'designToken/fontSizes';
+import { fontFamily } from 'designToken/fontFamily';
+import { colors } from 'designToken/colors';
+import Button from 'components/common/Button';
 
 const TreatmentEnd = () => {
   return (
-    <View>
-      <Text title={'Treatment is ended'} />
-      <View style={styles.container}>
-        <Image
-          source={require('../../../assets/icon/doctorIcon.png')}
-          style={styles.doctorIcon}
-        />
-        {/* <Image source={require("../../../assets/icon/star.png")} style={styles.starIcon} /> */}
+    <>
+      <Text title={'Treatment is ended'} style={styles.treatmentText} />
+      <View style={styles.imageContainer}>
+        <View style={styles.doctorIconContainer}>
+          <Image
+            source={require('../../../assets/icon/doctorIcon.png')}
+            style={styles.doctorIcon}
+          />
+          <Image
+            source={require('../../../assets/icon/star.png')}
+            style={styles.starIcon}
+          />
+          <Text style={styles.rating} title={'4.8'} />
+        </View>
         <View>
-          <Text title={'Elena Miron,\nFamily doctor'} />
-          <View style={{ flexDirection: 'row', gap: 20 }}>
+          <Text
+            title={'Elena Miron,\nFamily doctor'}
+            style={{ fontSize: getHeight(fontSize.textXl) }}
+          />
+          <View style={styles.container}>
             <Image
               source={require('../../../assets/icon/heart.png')}
               style={styles.heartIcon}
             />
-            <Text title={'Add to favourites'} />
+            <Text title={'Add to favourites'} style={styles.text} />
           </View>
         </View>
       </View>
-    </View>
+
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../../../assets/icon/cardboard.png')}
+          style={styles.doctorCard}
+        />
+
+        <View style={{ gap: 10 }}>
+          <Text title={'Order summary'} style={styles.order} />
+          <View style={{ gap: 8 }}>
+            <Text title={'Basic - 500 NIS'} style={styles.text} />
+            <Text title={'Voltaren shot - 100 NIS'} style={styles.text} />
+            <View style={{ flexDirection: 'row' }}>
+              <Text title={'Total'} style={styles.total} />
+              <Text title={' - 600 NIS'} style={styles.text} />
+            </View>
+          </View>
+        </View>
+      </View>
+      <Button
+        title={'Approve payment'}
+        isPrimary
+        isSmall
+        style={{ alignSelf: 'center' }}
+        width={'80%'}
+      />
+    </>
   );
 };
 
@@ -37,20 +76,61 @@ const styles = StyleSheet.create({
     height: getHeight(110),
     resizeMode: 'contain',
   },
+  doctorCard: {
+    width: getWidth(100),
+    height: getHeight(110),
+    resizeMode: 'contain',
+    marginLeft: getWidth(25),
+  },
   starIcon: {
     position: 'absolute',
-    bottom: getHeight(0),
-    left: getHeight(0),
+    bottom: getHeight(dimens.marginS),
+    right: getHeight(dimens.marginS),
     width: getWidth(dimens.marginM),
     height: getHeight(dimens.marginM),
   },
-  container: {
+  imageContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    gap: 30,
+    gap: getHeight(dimens.imageXs),
+    justifyContent: 'center',
+    marginBottom: getHeight(dimens.imageS + dimens.marginL),
   },
   heartIcon: {
     width: getWidth(dimens.marginM),
     height: getHeight(dimens.marginM),
+    resizeMode: 'center',
+  },
+  text: {
+    fontSize: getHeight(fontSize.textM),
+  },
+  treatmentText: {
+    textAlign: 'center',
+    fontSize: getHeight(20),
+    fontFamily: fontFamily.semiBold,
+    marginVertical: getHeight(dimens.imageXs),
+  },
+  container: {
+    flexDirection: 'row',
+    gap: getHeight(dimens.marginS),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  doctorIconContainer: {
+    position: 'relative',
+  },
+  order: {
+    fontSize: getHeight(20),
+  },
+  rating: {
+    position: 'absolute',
+    bottom: getHeight(-11),
+    right: getHeight(dimens.paddingXs + 2),
+    fontSize: getHeight(fontSize.textM),
+    color: colors.secondary,
+  },
+  total: {
+    fontFamily: fontFamily.medium,
+    fontSize: getWidth(fontSize.textM),
   },
 });
